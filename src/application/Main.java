2e518714a41ea765b384e117b58d01b9491e2542
Vinícius de Paula <manoseilacara123@gmail.com>
2024-12-27 19:48:04 -0300

    implement methods to get data

--- src/application/Main.java
+++ src/application/Main.java
@@ -3,10 +3,33 @@ package application;
 import db.DB;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Main {
     public static void main(String[] args) {
-        Connection connection = DB.getConnection();
-        DB.closeConnection();
+        Connection connection = null;
+        Statement st = null;
+        ResultSet rs = null;
+
+        try {
+            connection = DB.getConnection();
+
+            st = connection.createStatement();
+
+            rs = st.executeQuery("select * from department");
+
+            while (rs.next()) {
+                System.out.println(rs.getInt("Id") + ", " + rs.getString("Name"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        finally {
+            DB.closeResultSet(rs);
+            DB.closeStatement(st);
+            DB.closeConnection();
+        }
     }
 }
