2f6abc8d3ba84e560bbf151d5243ac427a131ee2
Vinícius de Paula <manoseilacara123@gmail.com>
2024-12-28 10:07:43 -0300

    implement methods to remove the data

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,27 +1,30 @@
 package application;
 
 import db.DB;
+import db.DbIntegrityException;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) {
         Connection connection = null;
         PreparedStatement st = null;
 
-        try {
+        try{
             connection = DB.getConnection();
+
             st = connection.prepareStatement(
-                    "UPDATE seller SET BaseSalary = BaseSalary + ?" +
-                            "WHERE (DepartmentId = ?)");
-            st.setDouble(1, 200.0);
-            st.setInt(2, 2);
+                    "DELETE FROM department WHERE Id = ?"
+            );
+            st.setInt(1, 6);
 
             int rowsAffected = st.executeUpdate();
 
             System.out.println("Done! Rows affected: " + rowsAffected);
         } catch (SQLException e) {
-            e.printStackTrace();
+            throw new DbIntegrityException(e.getMessage());
         } finally {
             DB.closeStatement(st);
             DB.closeConnection();
