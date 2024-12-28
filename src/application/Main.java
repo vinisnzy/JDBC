26bfc54f215a257285d3e7e5c7a3b51e2fa8e797
Vinícius de Paula <manoseilacara123@gmail.com>
2024-12-28 09:41:32 -0300

    implement methods to update data

--- src/application/Main.java
+++ src/application/Main.java
@@ -3,42 +3,24 @@ package application;
 import db.DB;
 
 import java.sql.*;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 
 public class Main {
     public static void main(String[] args) {
-
-        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
         Connection connection = null;
         PreparedStatement st = null;
 
         try {
             connection = DB.getConnection();
-
             st = connection.prepareStatement(
-                    "INSERT INTO seller (Name, Email, BirthDate, BaseSalary, DepartmentId)" +
-                            "VALUES (?, ?, ?, ?, ?)",
-                    Statement.RETURN_GENERATED_KEYS);
-            st.setString(1, "Vinícius de Paula");
-            st.setString(2, "vinicius@gmail.com");
-            st.setDate(3, new java.sql.Date(sdf.parse("19/01/2008").getTime()));
-            st.setDouble(4, 12000.0);
-            st.setInt(5, 1);
+                    "UPDATE seller SET BaseSalary = BaseSalary + ?" +
+                            "WHERE (DepartmentId = ?)");
+            st.setDouble(1, 200.0);
+            st.setInt(2, 2);
 
             int rowsAffected = st.executeUpdate();
 
-            if (rowsAffected > 0) {
-                ResultSet rs = st.getGeneratedKeys();
-                while (rs.next()) {
-                    int id = rs.getInt(1);
-                    System.out.println("Done! Id: " + id);
-                }
-            } else {
-                System.out.println("No rows affected!");
-            }
-
-        } catch (SQLException | ParseException e) {
+            System.out.println("Done! Rows affected: " + rowsAffected);
+        } catch (SQLException e) {
             e.printStackTrace();
         } finally {
             DB.closeStatement(st);
